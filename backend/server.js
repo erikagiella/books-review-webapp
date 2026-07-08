d44ecd09e99fe0290dd004ff11a2074dd1747b29
erikagiella <erikagiella93@gmail.com>
2026-07-08 18:35:16 +0200

    add error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express();
 const PORT = process.env.API_SERVER_PORT || 3000;
 const booksRouter = require("./routes/books");
+const serverError = require("./middlewares/serverError");
+const notFound = require("./middlewares/notFound");
+
 
 // register the static assets folder
 app.use(express.static("public"));
@@ -18,3 +21,9 @@ app.get("/", (req, res) => {
 
 // books router
 app.use("/api/v1/books", booksRouter);
+
+// handle server errors
+app.use(serverError);
+
+// catch-all route for 404 errors
+app.use(notFound);
